e002dfb1f3289b0e63c928dbb3e2d351d15b6ade
gordon <swissarthurfreeman@gmail.com>
2024-05-01 17:48:16 +0200

    feat(biscuits-ldap-server): added basic entry creations

--- biscuits-application/src/main/java/ch/unige/biscuits/rest/UserController.java
+++ biscuits-application/src/main/java/ch/unige/biscuits/rest/UserController.java
@@ -13,7 +13,6 @@ import ch.unige.biscuits.domain.User;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.lookup.Interpolator;
 
 @RestController
 @RequestMapping("/users")
