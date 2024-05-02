abfe2a3d24167dc5cdac57488d3185b1f5e64770
gordon <swissarthurfreeman@gmail.com>
2024-05-02 12:40:32 +0200

    feat(biscuits-app): Added simple LDAP authentication mechanism

---
+++ biscuits-application/src/main/java/ch/unige/biscuits/rest/ApplicationController.java
@@ -0,0 +1,15 @@
+package ch.unige.biscuits.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class ApplicationController {
+    @RequestMapping("/")
+    public ModelAndView welcome() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index.html");
+        return modelAndView;
+    }
+}
